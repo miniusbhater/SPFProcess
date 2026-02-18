8785ac08152a296ecf53aca0b25ed4d6da2639d7
Rose <miniusbhater@gmail.com>
2026-02-18 16:17:29 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("steam_api.dll")]
+    private static extern bool SteamAPI_Init();
+
+    [DllImport("steam_api.dll")]
+    private static extern void SteamAPI_RunCallbacks();
+
+    static void Main(string[] args)
+    {
+        if (args.Length == 0) // help you if your fucking stupid (jokes :p)
+        {
+            Console.WriteLine("Usage: SteamPlaytimeFarm.exe <AppID>");
+            return;
+        }
+
+        string appId = args[0];
+
+        File.WriteAllText("steam_appid.txt", appId); // i wonder
+
+        if (!SteamAPI_Init())
+        {
+            Console.WriteLine("Steam init failed.");
+            return;
+        }
+
+        Console.WriteLine($"Idling AppID {appId}");
+
+        while (true)
+        {
+            SteamAPI_RunCallbacks();
+            Thread.Sleep(1000);
+        }
+    }
+}
