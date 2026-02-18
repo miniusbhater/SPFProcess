52789f0d4becddddf2fba75484a6ff8595570b48
Rose <miniusbhater@gmail.com>
2026-02-18 19:38:03 +0000

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SteamPlaytimeFarm")]
+[assembly: AssemblyProduct("SteamPlaytimeFarm")]
+[assembly: AssemblyCompany("miniusbhater")]
+[assembly: AssemblyDescription("Farm Steam playtime")]
+[assembly: AssemblyCopyright("©2026 miniusbhater")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
